34e45968616951641cba69c6e66660b0c8434b4f
Aindrail <aindrailsantra@gmail.com>
2022-03-20 21:08:40 +0530

    CartItems container created

--- src/CartItems.js
+++ src/CartItems.js
@@ -1,4 +1,4 @@
-import React from 'react'
+ import React from 'react'
 import styled from 'styled-components'
 import CartItem from './CartItem'
 
@@ -23,12 +23,12 @@ function CartItems({ cartItems }) {
 
 export default CartItems
 
-const Container = styled.div`
-    flex: 0.8;
-    padding: 20px;
-    margin-right: 18px;
-    background-color: white;
-`
+// const Container = styled.div`
+//     flex: 0.8;
+//     padding: 20px;
+//     margin-right: 18px;
+//     background-color: white;
+// `
 
 const Title = styled.h1`
     margin-bottom: 8px;
