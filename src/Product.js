0169b799aa7d1cfd6141a76f9a029e2b3e15133c
Aindrail <aindrailsantra@gmail.com>
2022-03-20 20:51:28 +0530

    Database linked to product

--- src/Product.js
+++ src/Product.js
@@ -26,31 +26,31 @@ function Product({ title, price, rating, image, id }) {
         })
     }
 
-    // return (
-    //     <Container>
-    //         <Title>
-    //             {title}
-    //         </Title>
-    //         <Price>
-    //         ₹ {price}
-    //         </Price>
-    //         <Rating>
-    //             {
-    //                 Array(rating)
-    //                     .fill()
-    //                     .map(rating => <p>⭐</p>)
-    //             }
-    //         </Rating>
-    //         <Image src={image} />
-    //         <ActionSection>
-    //             <AddToCartButton
-    //                 onClick={addToCart}
-    //             >
-    //                  Add to Cart
-    //             </AddToCartButton>
-    //         </ActionSection>
-    //     </Container>
-    // )
+    return (
+        <Container>
+            <Title>
+                {title}
+            </Title>
+            <Price>
+            ₹ {price}
+            </Price>
+            <Rating>
+                {
+                    Array(rating)
+                        .fill()
+                        .map(rating => <p>⭐</p>)
+                }
+            </Rating>
+            <Image src={image} />
+            <ActionSection>
+                <AddToCartButton
+                    onClick={addToCart}
+                >
+                     Add to Cart
+                </AddToCartButton>
+            </ActionSection>
+        </Container>
+    )
 }
 
 export default Product
