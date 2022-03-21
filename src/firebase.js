a40e214b83ce1fbeef6bef28ba248c3b1bded7fb
Aindrail <aindrailsantra@gmail.com>
2022-03-21 18:30:04 +0530

    The auth and provider are expoted fro DB firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from 'firebase'
 
 const firebaseApp = firebase.initializeApp({
-    // apiKey: "AIzaSyBL6yj1-ZNcR6qvN22_CnNNmrD90ezz3vg",
-    // authDomain: "clone-mar21.firebaseapp.com",
-    // projectId: "clone-mar21",
-    // storageBucket: "clone-mar21.appspot.com",
-    // messagingSenderId: "781350103372",
-    // appId: "1:781350103372:web:a86eda03286270903707e9",
-    // measurementId: "G-4RQMTWZN67"
+    
     apiKey: "AIzaSyCXuBVvxmqzsINDav2qHRyFUbSFNteMZjY",
     authDomain: "clone-reactjs-aindrail.firebaseapp.com",
     projectId: "clone-reactjs-aindrail",
